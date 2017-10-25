a119ac54f7d12d4a676aaa97c828dafde07e75f5
Marc Laliberte <marclaliberte@gmail.com>
2017-10-25 13:02:51 -0700

    Added individual scripts for new servers, clients and storage

---
+++ broker/add_server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import pymongo
+import config
+import sys
+
+def handle_list(arg):
+    if arg:
+        return arg.split(",")
+    else:
+        return []
+
+if len(sys.argv) < 5:
+    print >> sys.stderr, "Usage: %s <ident> <secret>"%sys.argv[0]
+    sys.exit(1)
+
+ident = sys.argv[1]
+secret = sys.argv[2]
+
+rec = {
+    "identifier": ident,
+    "secret": secret,
+    "publish": ['artemis.raw','artemis.parsed','artemis.urls','artemis.attachments'],
+    "subscribe":['NONE']
+}
+
+client = pymongo.MongoClient(host=config.MONGOHOST,port=config.MONGOPORT)
+res = client.hpfeeds.auth_key.update({"identifier": ident}, {"$set": rec}, upsert=True)
+client.fsync()
+client.close()
+
+if res['updatedExisting']:
+    print "updated %s"%rec
+else:
+    print "inserted %s"%(rec)
+
---
+++ broker/add_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import pymongo
+import config
+import sys
+
+def handle_list(arg):
+    if arg:
+        return arg.split(",")
+    else:
+        return []
+
+if len(sys.argv) < 5:
+    print >> sys.stderr, "Usage: %s <ident> <secret>"%sys.argv[0]
+    sys.exit(1)
+
+ident = sys.argv[1]
+secret = sys.argv[2]
+publish = ['NONE']
+subscribe = ['thug.files','thug.events','artemis.parsed']
+
+rec = {
+    "identifier": ident,
+    "secret": secret,
+    "publish": publish,
+    "subscribe":subscribe
+}
+
+client = pymongo.MongoClient(host=config.MONGOHOST,port=config.MONGOPORT)
+res = client.hpfeeds.auth_key.update({"identifier": ident}, {"$set": rec}, upsert=True)
+client.fsync()
+client.close()
+
+if res['updatedExisting']:
+    print "updated %s"%rec
+else:
+    print "inserted %s"%(rec)
+
